25f30a51dca8a5c0f03449cbf822057ccd1b7398
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-10-31 12:57:08 +0900

    modify motice_mailer

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,5 +1,5 @@
 class NoticeMailer < ApplicationMailer
-  def notice_mail(blog)
+  def notice_mail(picture)
      @picture = picture
      @user = @picture.user
      mail to: @user.email, subject: "画像投稿の確認メール"
