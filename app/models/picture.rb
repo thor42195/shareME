a5584dc3bfe8ad654cb8e0bb03e90103a2215695
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-10-31 14:25:39 +0900

    add comfirm pasth before post

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
+  validates :content, presence:true
+  validates :postimage, presence:true
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
